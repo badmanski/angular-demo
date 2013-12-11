a2eb38eff26d4165c8b97c5fbdc5ca522b8a10de
badmanski <stanislav.gorski@gmail.com>
2013-12-11 20:46:37 +0200

    Create notes scaffold

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,29 @@
+class NotesController < ApplicationController
+  respond_to :json
+
+  before_action :set_note, only: [:update, :destroy]
+
+  def index
+  end
+
+  def create
+    respond_with Note.create(note_params)
+  end
+
+  def update
+    respond_with @note.update(note_params)
+  end
+
+  def destroy
+    respond_with @note.destroy
+  end
+
+  private
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    def note_params
+      params.require(:note).permit(:content)
+    end
+end
